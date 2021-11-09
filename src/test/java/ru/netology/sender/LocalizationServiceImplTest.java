8d2c38d1acd91db3f62f104c1ac003d2cfc41d15
Anatoly Balbutsky <bal.anatolii1985@mail.ru>
2021-11-09 21:30:14 +0300

    mockito4

---
+++ src/test/java/ru/netology/sender/LocalizationServiceImplTest.java
@@ -0,0 +1,28 @@
+package ru.netology.sender;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+import ru.netology.i18n.LocalizationService;
+import ru.netology.i18n.LocalizationServiceImpl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LocalizationServiceImplTest {
+
+LocalizationService localizationService= new LocalizationServiceImpl();
+
+
+    @Test
+    public void testReturnedTextRussia(){
+        String locale = localizationService.locale(Country.RUSSIA);
+        assertEquals("Добро пожаловать", locale);
+    }
+
+
+
+    @Test
+    public void testReturnedTextDefault(){
+        String locale = localizationService.locale(Country.USA);
+        assertEquals("Welcome", locale);
+    }
+}
